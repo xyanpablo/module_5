d25b06229f5518711aae9682cb3687c67b30236f
xyanpablo <xyanpablo671@gfmail.com>
2024-09-29 15:25:38 +0300

    module_5

---
+++ module_5_4.py
@@ -0,0 +1,41 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args):
+        cls. houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        new_floor = int(new_floor)
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print(f'Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+                i += 1
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+del h2
+del h3
+
+print(House.houses_history)
