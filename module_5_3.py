db1ebe197d086f5120d72b4bc5d4dfa9fc00d013
xyanpablo <xyanpablo671@gfmail.com>
2024-10-03 22:35:41 +0300

    module_5

--- module_5_3.py
+++ module_5_3.py
@@ -19,31 +19,40 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        return self.__eq__(other) or self.__lt__(other)
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, House):
+            return self.number_of_floors > other
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        return self.__eq__(other) or self.__gt__(other)
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        return not self.__eq__(other)
 
-    def __add__(self, value):
-        return House(self.name, self.number_of_floors + value)
+    def __add__(self, other):
+        return House(self.name, self.number_of_floors + other)
 
-    def __iadd__(self, value):
-        return self.__add__(value)
+    def __iadd__(self, other):
+        return self.__add__(other)
 
-    def __radd__(self, value):
-        return self.__add__(value)
+    def __radd__(self, other):
+        return self.__add__(other)
 
 
 h1 = House('ЖК Эльбрус', 10)
