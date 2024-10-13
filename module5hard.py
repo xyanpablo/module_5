6fb036fe10da24dbb27e63f5bb37eb01d2d91b9c
xyanpablo <xyanpablo671@gfmail.com>
2024-10-13 22:39:02 +0300

    module_5

--- module5hard.py
+++ module5hard.py
@@ -10,22 +10,21 @@ class User:
     def __hash__(self):
         return hash(self.password)
 
-
     def __str__(self):
         return self.nickname
 
     def __eq__(self, other):
         if not isinstance(other, (str, User)):
-            raise TypeError("Операнд должен иметь тип 'str' или 'User'")
+            raise TypeError('Операнд должен иметь тип "str" или "User"')
         res = other if isinstance(other, str) else other.nickname
         return self.nickname == res
 
-
     def __lt__(self, other):
         if not isinstance(other, (int, User)):
-            raise TypeError("Операнд должен иметь тип 'int' или 'User'")
+            raise TypeError('Операнд должен иметь тип "int" или "User"')
         res = other if isinstance(other, int) else other.age
         return self.age < res
+
 
 class Video:
     def __init__(self, title, duration, time_now=0, adult_mode=False):
@@ -34,12 +33,12 @@ class Video:
         self.time_now = time_now
         self.adult_mode = adult_mode
 
-
     def __str__(self):
         return self.title
 
     def __eq__(self, other):
         return self.title == other.title
+
 
 class UrTube:
     def __init__(self):
@@ -47,75 +46,68 @@ class UrTube:
         self.videos = []
         self.current_user = None
 
-
-    def __eq__(self, other):
-        if not isinstance(other, (str, Video)):
-            raise TypeError("Операнд должен иметь тип 'str' или 'Video'")
-        res = other if isinstance(other, str) else other.title
-        return self.title == res
-
     def __str__(self):
-        """Метод для функций print, format возвращает строку,
-        содержащую названия всех video из списка videos"""
-        name_video = ""
-        for video in self.videos:
-            name_video += video.title + '\n'
-        name_video = name_video[ : len(name_video) - 1]
+        name_video = '\n'.join(video.title for video in self.videos)
         return name_video
-    def log_in(self, nickname, password):
-        if nickname in self.users:
-            if hash(password) == User.__hash__(password):
-                self.current_user = User
-                print(f'{nickname}, вход выполнен.')
-            else:
-                print('Неверный пароль.')
 
-        else:
+    def log_in(self, nickname, password):
+        for i in self.users:
+            if i.nickname == nickname:
+                if i.password == hash(password):
+                    self.current_user = User
+                    print(f'{nickname}, вход выполнен.')
+                    return
+                else:
+                    print('Неверный пароль.')
+                    return
             print('Такого пользователя не существует.')
 
     def register(self, nickname, password, age):
         for i in self.users:
-            if nickname == i:
+            if i.nickname == nickname:
                 print(f'Пользователь с никнеймом: {nickname} уже существует.')
-            else:
-                self.users.append(User(nickname, password, age))
-                self.log_in(nickname, password)
-                print(f'Регистрация успешно выполнена, {nickname}')
+                return
+        self.users.append(User(nickname, password, age))
+        print(f'Регистрация успешно выполнена, {nickname}')
+        self.log_in(nickname, password)
 
     def log_out(self):
         self.current_user = None
 
     def add(self, *video):
-        if video in self.videos:
-            pass
-        else:
-            self.videos.append(video)
+        for i in video:
+            if i in self.videos:
+                pass
+            else:
+                self.videos.append(i)
 
-    def get_videos(self, key):
+    def get_videos(self, keyword):
         search = []
         for i in self.videos:
-            if key.lower() in i.title.lower():
+            if keyword.lower() in i.title.lower():
                 search.append(i.title)
         return search
 
-    def watch_video(self, mov):
+    def watch_video(self, title):
         if self.current_user is None:
             print("Войдите в аккаунт, чтобы смотреть видео")
-        else:
-            for i in self.videos:
-                if mov == i:
-                    if i.adult_mode and self.current_user.age < 18:
-                        print("Вам нет 18 лет, пожалуйста покиньте страницу")
-                    else:
-                        for j in range(i.duration):
-                            i.time_now += 1
-                            sleep(1)
-                            print(i.time_now)
-                        print("Конец видео")
-                else:
-                    continue
-            Video.time_now = 0
+            return
+        for i in self.videos:
+            if title == i.title:
+                if i.adult_mode and self.current_user.age < 18:
+                    print("Вам нет 18 лет, пожалуйста покиньте страницу")
+                    return
+                for j in range(i.duration):
+                    i.time_now += 1
+                    sleep(1)
+                    print(i.time_now)
+                print("Конец видео")
+                i.time_now = 0
+                return
+        print('Видео не найдено')
 
+
+# Пример использования класса UrTube
 ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
 v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
